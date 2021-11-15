cbbfd9475258ef71a5ed57f1760a06cd76dbbafc
AndriiPyatnitskyi <pyatnitskiy.a@gmail.com>
2021-11-16 00:21:09 +0200

    init

---
+++ src/controller/accounts.ts
@@ -0,0 +1,199 @@
+import {Request, Response} from 'express';
+import * as fs from 'fs';
+// import {Role, Account} from "../dto/account";
+
+const jwt = require("jsonwebtoken");
+const secretKey = "mySecretKey";
+// const models = require("../server");
+const Account = require("../server");
+const filePath = "./accounts.json";
+
+const getAccounts: any = async (req: Request, res: Response) => {
+    // const result = await models.default.AccountModel.findAll();
+    let find = await Account.default.find();
+    res.send(find);
+};
+
+const getAccountById: any = async (req: Request, res: Response) => {
+    const result = await Account.default.findOne({id: req.params.id});
+
+    if (result) {
+        res.send(result);
+    } else {
+        res.status(404).send();
+    }
+};
+
+// const createAccount: any = async (req: Request, res: Response) => {
+//     if (!req.body) return res.sendStatus(400);
+//
+//     const accountName: String = req.body.name;
+//     const accountRole: Role = req.body.role;
+//
+//     const role = accountRole ? accountRole : Role.USER;
+//
+//     const token = jwt.sign(
+//         {account_name: accountName, role: accountRole},
+//         secretKey,
+//         {
+//             expiresIn: "2h",
+//         }
+//     );
+//
+//     const result = await models.default.AccountModel.create({name: accountName, token: token, role: role});
+//
+//     res.send(result);
+// };
+//
+// const updateAccount: any = async (req: Request, res: Response) => {
+//     if (!req.body) return res.sendStatus(400);
+//
+//     let result = await models.default.AccountModel.update(
+//         {
+//             name: req.body.name
+//         },
+//         {
+//             where: {
+//                 id: req.params.id
+//             }
+//         });
+//
+//     if (result == 1) {
+//         res.send(await models.default.AccountModel.findByPk(req.params.id));
+//     } else {
+//         res.status(404).send(req.params.id);
+//     }
+// };
+//
+// const deleteAccount: any = async (req: Request, res: Response) => {
+//     let result = await models.default.AccountModel.destroy(
+//         {
+//             where: {
+//                 id: req.params.id
+//             }
+//         });
+//
+//     if (result == 1) {
+//         res.sendStatus(204);
+//     } else {
+//         res.status(404).send();
+//     }
+// };
+//
+// const getAccountTokensByAccountId: any = async (req: Request, res: Response) => {
+//     const result = await models.default.AccountModel.findByPk(req.params.id);
+//
+//     if (result) {
+//         res.send(result.token);
+//     } else {
+//         res.status(404).send();
+//     }
+// };
+//
+// const createAccountToken: any = async (req: Request, res: Response) => {
+//     if (!req.body) return res.sendStatus(400);
+//
+//     const account = await models.default.AccountModel.findByPk(req.params.id);
+//
+//     if (!account) {
+//         res.status(404).send(req.params.id);
+//     }
+//
+//     const updated = await models.default.AccountModel.update(
+//         {
+//             token: jwt.sign(
+//                 {account_name: account.name},
+//                 secretKey,
+//                 {
+//                     expiresIn: "2h",
+//                 }
+//             )
+//         },
+//         {
+//             where: {
+//                 id: req.params.id
+//             }
+//         });
+//
+//     if (updated == 1) {
+//         res.send(await models.default.AccountModel.findByPk(req.params.id));
+//     } else {
+//         res.sendStatus(500);
+//     }
+// };
+//
+// const updateAccountToken: any = async (req: Request, res: Response) => {
+//     if (!req.body) return res.sendStatus(400);
+//
+//     const accountSourceToken: String = req.body.sourceToken;
+//     const accountTargetToken: String = req.body.targetToken;
+//
+//     const account = await models.default.AccountModel.findByPk(req.params.id);
+//
+//     if (!account) {
+//         res.status(404).send(req.params.id);
+//     }
+//
+//     if (account.token !== accountSourceToken) {
+//         res.status(404).send(req.params.id);
+//     }
+//
+//
+//     const updated = await models.default.AccountModel.update(
+//         {
+//             token: accountTargetToken
+//         },
+//         {
+//             where: {
+//                 id: req.params.id,
+//                 token: accountSourceToken
+//             }
+//         });
+//
+//     if (updated == 1) {
+//         res.send(await models.default.AccountModel.findByPk(req.params.id));
+//     } else {
+//         res.sendStatus(500);
+//     }
+// };
+//
+// const deleteAccountToken: any = async (req: Request, res: Response) => {
+//     if (!req.body) return res.sendStatus(400);
+//
+//     const account = await models.default.AccountModel.findByPk(req.params.id);
+//
+//     if (!account) {
+//         res.status(404).send(req.params.id);
+//     }
+//
+//     const updated = await models.default.AccountModel.update(
+//         {
+//             token: ""
+//         },
+//         {
+//             where: {
+//                 id: req.params.id
+//             }
+//         });
+//
+//
+//     if (updated == 1) {
+//         res.send(await models.default.AccountModel.findByPk(req.params.id));
+//     } else {
+//         res.sendStatus(500);
+//     }
+// };
+
+export default {
+    getAccounts,
+    getAccountById
+    // ,
+    // createAccount,
+    // updateAccount,
+    // deleteAccount,
+    // getAccountTokensByAccountId,
+    // createAccountToken,
+    // updateAccountToken,
+    // deleteAccountToken
+};
+
---
+++ src/server.ts
@@ -0,0 +1,95 @@
+import http from 'http';
+import express, {Express} from 'express';
+import morgan from 'morgan';
+import accountRouter from './routes/accounts';
+import tokenRouter from './routes/tokens';
+import swaggerUi from 'swagger-ui-express';
+import * as path from "path";
+const mongoose = require("mongoose");
+
+
+// const Sequelize = require('sequelize');
+// const accountModel = require("./model/account");
+
+const swaggerDocument = require('./swagger.json');
+const app: Express = express();
+
+/** Init DB */
+// const connection = new Sequelize('postgres://habrpguser:pgpwd4habr@localhost:5432/habrdb') // Example for postgres
+
+// connection.authenticate()
+//     .then(() => {
+//     console.log('Connection has been established successfully.');
+// }).catch((err: any) => {
+//     console.error('Unable to connect to the database:', err);
+// });
+//
+// Init tables
+// const AccountModel = accountModel(connection);
+
+// AccountModel.sync();
+
+// load models
+
+// const models = {
+//     AccountModel: require(path.join(__dirname, './model/account'))(connection, Sequelize.DataTypes),
+//     sequelize: connection,
+//     Sequelize: Sequelize
+// }
+
+const Schema = mongoose.Schema;
+
+// установка схемы
+const userScheme = new Schema({
+    name: String,
+    token: String,
+    role: String
+});
+
+// подключение
+mongoose.connect("mongodb://localhost:27017/usersdb", { useUnifiedTopology: true, useNewUrlParser: true });
+
+const Account = mongoose.model("Account", userScheme);
+
+/** Swagger */
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+/** Logging */
+app.use(morgan('dev'));
+/** Parse the request */
+app.use(express.urlencoded({extended: false}));
+/** Takes care of JSON data */
+app.use(express.json());
+
+/** RULES OF OUR API */
+app.use((req, res, next) => {
+    // set the CORS policy
+    res.header('Access-Control-Allow-Origin', '*');
+    // set the CORS headers
+    res.header('Access-Control-Allow-Headers', 'origin, X-Requested-With,Content-Type,Accept, Authorization');
+    // set the CORS method headers
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'GET PATCH DELETE POST');
+        return res.status(200).json({});
+    }
+    next();
+});
+
+/** Routes */
+app.use('/', accountRouter);
+app.use('/', tokenRouter);
+
+/** Error handling */
+app.use((req, res, next) => {
+    const error = new Error('not found');
+    return res.status(404).json({
+        message: error.message
+    });
+});
+
+/** Server */
+const httpServer = http.createServer(app);
+const PORT: any = process.env.PORT ?? 6062;
+httpServer.listen(PORT, () => console.log(`The server is running on port ${PORT}`));
+
+export { Account as default }
